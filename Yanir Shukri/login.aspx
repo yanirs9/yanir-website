c28b72db3650f36350cd8ae61b6d2c9f139c2cea
yanir <yanirsh2010@gmail.com>
2026-01-11 15:37:00 +0200

    DB

---
+++ Yanir Shukri/login.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="כניסה" Language="C#" MasterPageFile="~/site.master" AutoEventWireup="true" CodeFile="login.aspx.cs" Inherits="MySite.Login" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>כניסת משתמשים</h2>
+    <hr />
+    
+    <div>
+        שם משתמש: <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+        <br /><br />
+        
+        סיסמה: <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+        <br /><br />
+        
+        <asp:Button ID="btnLogin" runat="server" Text="התחבר" OnClick="btnLogin_Click" />
+        <br /><br />
+        
+        <asp:Label ID="lblLoginMessage" runat="server" Text="" ForeColor="Red"></asp:Label>
+    </div>
+</asp:Content>
---
+++ Yanir Shukri/login.aspx.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MySite
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            // בדיקה פשוטה לצורך בדיקת התיקון
+            if (txtUsername.Text == "admin" && txtPassword.Text == "1234")
+            {
+                Session["user"] = txtUsername.Text;
+                Response.Redirect("Default.aspx");
+            }
+            else
+            {
+                lblLoginMessage.Text = "פרטים שגויים!";
+            }
+        }
+    }
+}
